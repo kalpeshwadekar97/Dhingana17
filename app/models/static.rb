af49aea9a30c1f0a5af62c4d757a8dbebcd0c3e8
shubhadip <shubhadip.maity@gmail.com>
2017-07-10 11:40:11 +0530

    changes in home api

--- app/models/static.rb
+++ app/models/static.rb
@@ -1,12 +1,17 @@
 class Static 
-
   def self.home
-    data = [
-      { id: 1, url:'http://lorempixel.com/400/200/', title:'banner'},
-      { id: 2, url:'http://lorempixel.com/400/200/', title:'roads'},
-      { id: 3, url:'http://lorempixel.com/400/200/', title:'places'},
-      { id: 4, url:'http://lorempixel.com/400/200/', title:'restuarants'}
-    ]
+    data = {
+      "home_banner":{
+          id: 1,
+          url: "http://lorempixel.com/800/600/"
+        },
+      "home":[
+          { id: 1, url:'http://lorempixel.com/400/200/', title:'banner'},
+          { id: 2, url:'http://lorempixel.com/400/200/', title:'roads'},
+          { id: 3, url:'http://lorempixel.com/400/200/', title:'places'},
+          { id: 4, url:'http://lorempixel.com/400/200/', title:'restuarants'}
+        ]
+      }
   end
 
   def self.faq
