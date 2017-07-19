8833efe64a48fb6ce6d8aaeda7c812f7b52179c3
shubhadip <shubhadip.maity@gmail.com>
2017-07-19 19:01:29 +0530

    description key added

--- app/models/static.rb
+++ app/models/static.rb
@@ -20,23 +20,33 @@ class Static
   end
 
   def self.roads
-    {title: 'Roads',data: STATIC_CONFIG["roads"] }
+    {title: 'Roads',description: 'Sacred travel, or metaphysical tourism, spiritualized travel, is a growing niche of the travel market. It attracts New Age believers and involves tours and travel to spiritual hotspots on the Earth.
+            Destinations are often ancient sites where there is a mystery concerning their origin or purpose, such as Shitaladevi Temple, choul. Birla Mandir, Visrgi Mosque, Adhal. Siddhivinayak Temple. 
+            Travelling through these routes, travelers see the journey as more than just tourism and take the trips in order to heal themselves . Part of this may involve rituals involving (supposedly) leaving their bodies, possession by spirits , and recovery of past life memories.', data: STATIC_CONFIG["roads"] }
   end
 
   def self.beaches
-    {title: 'Beaches',data: STATIC_CONFIG["beaches"] }
+    {title: 'Beaches',description: 'Sacred travel, or metaphysical tourism, spiritualized travel, is a growing niche of the travel market. It attracts New Age believers and involves tours and travel to spiritual hotspots on the Earth.
+            Destinations are often ancient sites where there is a mystery concerning their origin or purpose, such as Shitaladevi Temple, choul. Birla Mandir, Visrgi Mosque, Adhal. Siddhivinayak Temple. 
+            Travelling through these routes, travelers see the journey as more than just tourism and take the trips in order to heal themselves . Part of this may involve rituals involving (supposedly) leaving their bodies, possession by spirits , and recovery of past life memories.', data: STATIC_CONFIG["beaches"] }
   end
 
   def self.history
-    {title: 'Historical Places',data: STATIC_CONFIG["historical"] }
+    {title: 'Historical Places',description: 'Sacred travel, or metaphysical tourism, spiritualized travel, is a growing niche of the travel market. It attracts New Age believers and involves tours and travel to spiritual hotspots on the Earth.
+            Destinations are often ancient sites where there is a mystery concerning their origin or purpose, such as Shitaladevi Temple, choul. Birla Mandir, Visrgi Mosque, Adhal. Siddhivinayak Temple. 
+            Travelling through these routes, travelers see the journey as more than just tourism and take the trips in order to heal themselves . Part of this may involve rituals involving (supposedly) leaving their bodies, possession by spirits , and recovery of past life memories.', data: STATIC_CONFIG["historical"] }
   end
 
   def self.spiritual
-    {title: 'Spiritual Places',data: STATIC_CONFIG["spiritual"] }
+    {title: 'Spiritual Places', description: 'Sacred travel, or metaphysical tourism, spiritualized travel, is a growing niche of the travel market. It attracts New Age believers and involves tours and travel to spiritual hotspots on the Earth.
+            Destinations are often ancient sites where there is a mystery concerning their origin or purpose, such as Shitaladevi Temple, choul. Birla Mandir, Visrgi Mosque, Adhal. Siddhivinayak Temple. 
+            Travelling through these routes, travelers see the journey as more than just tourism and take the trips in order to heal themselves . Part of this may involve rituals involving (supposedly) leaving their bodies, possession by spirits , and recovery of past life memories.', data: STATIC_CONFIG["spiritual"] }
   end
 
   def self.wildlife
-    {title: 'Wildlife & Bird Sanctuaries',data: STATIC_CONFIG["wildlife"] }
+    {title: 'Wildlife & Bird Sanctuaries', description: 'Sacred travel, or metaphysical tourism, spiritualized travel, is a growing niche of the travel market. It attracts New Age believers and involves tours and travel to spiritual hotspots on the Earth.
+            Destinations are often ancient sites where there is a mystery concerning their origin or purpose, such as Shitaladevi Temple, choul. Birla Mandir, Visrgi Mosque, Adhal. Siddhivinayak Temple. 
+            Travelling through these routes, travelers see the journey as more than just tourism and take the trips in order to heal themselves . Part of this may involve rituals involving (supposedly) leaving their bodies, possession by spirits , and recovery of past life memories.', data: STATIC_CONFIG["wildlife"] }
   end
 
   def self.terms_and_conditions
