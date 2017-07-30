5d6c147d762fcf1e66077fffbe12ca826fa03909
shubhadip <shubhadip.maity@gmail.com>
2017-07-30 11:28:32 +0530

    bugfix

--- app/models/static.rb
+++ app/models/static.rb
@@ -6,11 +6,11 @@ class Static
           url: "https://s3.ap-south-1.amazonaws.com/dhingana/home_banner.jpg"
         },
       "home":[
-          { id: 1, url:'https://s3.ap-south-1.amazonaws.com/dhingana/home_boxes_beaches.jpg', title:'beaches'},
-          { id: 2, url:'https://s3.ap-south-1.amazonaws.com/dhingana/home_boxes_roads.jpg', title:'roads'},
-          { id: 3, url:'https://s3.ap-south-1.amazonaws.com/dhingana/home_boxes_spirituals.jpg', title:'spiritual places'},
-          { id: 4, url:'https://s3.ap-south-1.amazonaws.com/dhingana/home_boxes_sancturies.jpg', title:'scanturies'},
-          { id: 5, url:'https://s3.ap-south-1.amazonaws.com/dhingana/home_boxes_forts.jpg', title:'forts'}
+          { id: 1, url:'https://s3.ap-south-1.amazonaws.com/dhingana/Home/home_boxes_beaches.jpg', title:'beaches'},
+          { id: 2, url:'https://s3.ap-south-1.amazonaws.com/dhingana/Home/home_boxes_roads.jpg', title:'roads'},
+          { id: 3, url:'https://s3.ap-south-1.amazonaws.com/dhingana/Home/home_boxes_spirituals.jpg', title:'spiritual places'},
+          { id: 4, url:'https://s3.ap-south-1.amazonaws.com/dhingana/Home/home_boxes_sancturies.jpg', title:'scanturies'},
+          { id: 5, url:'https://s3.ap-south-1.amazonaws.com/dhingana/Home/home_boxes_forts.jpg', title:'forts'}
         ]
       }
   end
